653a21d82a380e5c0dd7cf34b77fd91ad1b946be
neoBontia <bon21004@byui.edu>
2021-12-15 02:06:31 +0800

    Added css and js to hamburger menu

--- scoots/js/general/scripts.js
+++ scoots/js/general/scripts.js
@@ -6,3 +6,18 @@ let date = new Date();
 
 document.getElementById("year").textContent = date.getFullYear();
 document.getElementById("latest-update").textContent = days[date.getDay()] + ", " + date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear();
+
+// For the small nav menu
+function toggleMenu() {
+    document.getElementById("navBar").classList.toggle("hide");
+
+    if (!document.getElementById("navBar").classList.contains("hide")) {
+        document.querySelector("nav").style.backgroundColor = "var(--main-color)";
+        document.querySelector("nav").style.borderRadius = "0px 0px 20px 20px";
+        document.querySelector("header").style.border = "none";
+    }
+    else {
+        document.querySelector("nav").style.backgroundColor = "white";
+        document.querySelector("header").style.borderBottom = "1px solid rgba(0, 0, 0, .8)";
+    }
+}
